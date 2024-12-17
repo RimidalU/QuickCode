44346e2074121eb8b796e714afe3a5da80813c63
uladzimir <stankevichuuladimiru@gmail.com>
2024-12-17 11:34:13 +0300

    feat: implement button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import clsx from 'clsx'
 
-const Button = () => {
-    return <button>Button</button>
+interface IButtonProps {
+    children: React.ReactNode
+    onClick: () => void
+    disabled?: boolean
+    className?: string
+}
+const Button = ({
+    className,
+    onClick,
+    disabled = false,
+    children,
+}: IButtonProps) => {
+    return (
+        <button
+            className={clsx(
+                'w-fit font-bold border text-xl px-3 py-1 rounded-md',
+                className
+            )}
+            onClick={onClick}
+            disabled={disabled}
+        >
+            {children}
+        </button>
+    )
 }
 
 export default Button
