5209af2038c89f1bea9af539538db55dcf83d251
uladzimir <stankevichuuladimiru@gmail.com>
2024-12-17 09:05:23 +0300

    feat: add inner components to code-editor section

--- src/app/components/CodeEditor.tsx
+++ src/app/components/CodeEditor.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import clsx from 'clsx'
+import SectionHeader from './SectionHeader'
+import LanguageSelector from './LanguageSelector'
+import CodeInput from './CodeInput'
 
-function CodeEditor() {
-    return <div>CodeEditor</div>
+interface ICodeEditorProps {
+    className?: string
+}
+
+const CodeEditor = ({ className }: ICodeEditorProps) => {
+    return (
+        <section className={clsx('flex flex-col w-1/2', className)}>
+            <SectionHeader />
+            <LanguageSelector />
+            <CodeInput />
+        </section>
+    )
 }
 
 export default CodeEditor
