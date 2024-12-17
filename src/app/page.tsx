ac8187208baa567cce376ca23511540567fd7530
uladzimir <stankevichuuladimiru@gmail.com>
2024-12-17 23:00:57 +0300

    feat: change mobile layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Result from '../components/Result'
 export default function Home() {
     return (
         <div className="items-center justify-items-center h-screen">
-            <main className="flex flex-col gap-8 items-center sm:items-start p-4 sm:p-8 sm:px-10 sm:flex-row w-full h-full">
+            <main className="flex flex-col gap-8 items-center md:items-start p-4 md:p-8 md:px-10 md:flex-row w-full max-w-screen-2xl h-full">
                 <CodeEditor />
                 <Result />
             </main>
