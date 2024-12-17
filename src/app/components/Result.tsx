1a3a1f1eef58c251266395b24720633a50687d07
uladzimir <stankevichuuladimiru@gmail.com>
2024-12-17 08:54:36 +0300

    feat: add inner components to result section

--- src/app/components/Result.tsx
+++ src/app/components/Result.tsx
@@ -1,7 +1,21 @@
+import clsx from 'clsx'
 import React from 'react'
+import SectionHeader from './SectionHeader'
+import Button from './Button'
+import OutputViewer from './OutputViewer'
 
-function Result() {
-    return <div>Result</div>
+interface IResultProps {
+    className?: string
+}
+
+const Result = ({ className }: IResultProps) => {
+    return (
+        <section className={clsx('flex flex-col w-1/2', className)}>
+            <SectionHeader />
+            <Button />
+            <OutputViewer />
+        </section>
+    )
 }
 
 export default Result
