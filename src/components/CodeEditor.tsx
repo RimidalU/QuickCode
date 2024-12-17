4cef7bcba006859c0c25d69851040aa2cf0ba9cf
uladzimir <stankevichuuladimiru@gmail.com>
2024-12-17 12:38:49 +0300

    feat: add languages-selector component to app

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,18 +1,35 @@
+'use client'
+
 import clsx from 'clsx'
+import { useState } from 'react'
+
+import { LANGUAGES, SectionTitles } from '../models/common.model'
+
 import SectionHeader from './SectionHeader'
 import LanguageSelector from './LanguageSelector'
 import CodeInput from './CodeInput'
-import { SectionTitles } from '../models/common.model'
 
 interface ICodeEditorProps {
     className?: string
 }
 
 const CodeEditor = ({ className }: ICodeEditorProps) => {
+    const [selectedLanguage, setSelectedLanguage] = useState(LANGUAGES[0])
+
+    const handleLanguageChange = (language: string) => {
+        setSelectedLanguage(language)
+    }
+
     return (
-        <section className={clsx('flex flex-col w-1/2 h-full', className)}>
+        <section
+            className={clsx('flex flex-col gap-2 w-1/2 h-full', className)}
+        >
             <SectionHeader title={SectionTitles.CodeEditor} />
-            <LanguageSelector />
+            <LanguageSelector
+                options={LANGUAGES}
+                onChange={handleLanguageChange}
+                selectedLanguage={selectedLanguage}
+            />
             <CodeInput />
         </section>
     )
