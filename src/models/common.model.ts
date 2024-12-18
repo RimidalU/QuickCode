034ac09b26055b0fdab10b439971a882fe096937
uladzimir <stankevichuuladimiru@gmail.com>
2024-12-18 14:37:04 +0300

    feat: implement language-info interface

--- src/models/common.model.ts
+++ src/models/common.model.ts
@@ -3,8 +3,25 @@ enum SectionTitles {
     Result = 'Output',
 }
 
-const LANGUAGES = ['javascript', 'python', 'java', 'typescript', 'php']
-type LanguageKeys = (typeof LANGUAGES)[number]
+interface LanguageInfo {
+    language: string
+    version: string
+    aliases: string[]
+}
 
-export { SectionTitles, LANGUAGES }
-export type { LanguageKeys }
+const DEFAULT_LANGUAGES = [
+    {
+        language: 'javascript',
+        version: '22.5.0',
+        aliases: ['node-javascript', 'node-js', 'javascript', 'js'],
+    },
+    // { language: 'python', version: '22.5.0' },
+    // { language: 'java', version: '22.5.0' },
+    // { language: 'typescript', version: '22.5.0' },
+    // { language: 'php', version: '22.5.0' },
+]
+
+// type LanguageKeys =  typeof LANGUAGES[number]['language']
+
+export { SectionTitles, DEFAULT_LANGUAGES }
+export type { LanguageInfo }
