572c205f7a26bf5c4f620ba0129b33edff3633be
uladzimir <stankevichuuladimiru@gmail.com>
2024-12-19 00:19:22 +0300

    fix: add padding to home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ const HomePageLayout = ({
 }) => {
     return (
         <div className="items-center justify-items-center h-screen">
-            <main className="flex flex-col gap-8 items-center md:items-start p-4 md:p-8 md:px-10 md:flex-row w-full max-w-screen-2xl h-full">
+            <main className="flex flex-col gap-8 items-center md:items-start p-4 md:p-8 md:px-10 md:flex-row w-full max-w-screen-2xl h-full pb-6">
                 {children}
             </main>
             {footer}
