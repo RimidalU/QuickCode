6e61cc193ef2520bee677ee246009c89993a9e82
uladzimir <stankevichuuladimiru@gmail.com>
2024-12-18 11:55:56 +0300

    refactor: add footer to home page props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,31 @@
+import { ReactNode } from 'react'
+
 import CodeEditor from '../components/CodeEditor'
 import Footer from '../components/Footer'
 import Result from '../components/Result'
 
 export default function Home() {
     return (
+        <HomePageLayout footer={<Footer />}>
+            <CodeEditor />
+            <Result />
+        </HomePageLayout>
+    )
+}
+
+const HomePageLayout = ({
+    children,
+    footer,
+}: {
+    children: ReactNode[]
+    footer: ReactNode
+}) => {
+    return (
         <div className="items-center justify-items-center h-screen">
             <main className="flex flex-col gap-8 items-center md:items-start p-4 md:p-8 md:px-10 md:flex-row w-full max-w-screen-2xl h-full">
-                <CodeEditor />
-                <Result />
+                {children}
             </main>
-            <Footer />
+            {footer}
         </div>
     )
 }
