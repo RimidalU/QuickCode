a7d818e8ddff5ff2a05b49772f3df8799712a367
uladzimir <stankevichuuladimiru@gmail.com>
2024-12-18 14:25:22 +0300

    refactor: change editor snipets

--- src/models/editor.model.ts
+++ src/models/editor.model.ts
@@ -1,8 +1,6 @@
-import { LanguageKeys } from './common.model'
-
 const MONACO_THEMES = ['vs-dark', 'light']
 
-const LANGUAGES_SNIPPETS: Record<LanguageKeys, string> = {
+const LANGUAGES_SNIPPETS: Record<string, string> = {
     javascript: `\nfunction getRandomNumber() {\n\treturn Math.floor(Math.random() * 100) + 1;\n}\n\nconsole.log(getRandomNumber());\n`,
     python: `\nimport random\n\ndef get_random_number():\n\treturn random.randint(1, 100)\n\nprint(get_random_number())\n`,
     java: `\nimport java.util.Random;\n\npublic class RandomNumber {\n\tpublic static void main(String[] args) {\n\t\tRandom rand = new Random();\n\t\tint randomNum = rand.nextInt(100) + 1;\n\t\tSystem.out.println(randomNum);\n\t}\n}\n`,
