ab4980b97e0e5e9da9190003c69aff7d850af817
uladzimir <stankevichuuladimiru@gmail.com>
2024-12-18 11:36:04 +0300

    feat: add base display result

--- src/components/OutputViewer.tsx
+++ src/components/OutputViewer.tsx
@@ -1,5 +1,17 @@
-const OutputViewer = () => {
-    return <div className={'border flex-grow h-full'}>OutputViewer</div>
+import clsx from 'clsx'
+
+interface IOutputViewerProps {
+    className?: string
+    result: string | null
+}
+const OutputViewer = ({ result, className }: IOutputViewerProps) => {
+    return (
+        <div className={clsx('border flex-grow h-full', className)}>
+            {result
+                ? result
+                : '// Select "Run Code" to check the results here.'}
+        </div>
+    )
 }
 
 export default OutputViewer
