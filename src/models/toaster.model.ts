dcd8d6f585d94210fea153b879e7fe081132b58f
uladzimir <stankevichuuladimiru@gmail.com>
2024-12-20 01:59:33 +0300

    feat: implement toaster model

---
+++ src/models/toaster.model.ts
@@ -0,0 +1,12 @@
+const TOAST_OPTIONS = {
+    classNames: {
+        title: 'text-warning text-sm',
+        error: 'text-red-500 bg-gray-50',
+    },
+}
+
+const TOASTER_MESSAGES = {
+    fetchingError:
+        'An error occurred while connecting to the Piston service. Please try again later.',
+}
+export { TOAST_OPTIONS, TOASTER_MESSAGES }
